a3f78ef2a2f68fc4d9a34309e7006d159fe72ed0
joaorobson <joaorobsonmartins@gmail.com>
2025-03-18 21:22:29 -0300

    Funcionalidade finalizada

--- hello_world.c
+++ hello_world.c
@@ -1,11 +1,19 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-int main(){int c = 10;
+int main(){
+    
+    /// CÓDIGO PRONTO PRA IR PRA MAIN
+
+    int variavel = 42;
+    int c = 10;
 
     printf("%d\n", c*20); // Modified multiplication factor
     printf("Hello from branchA\n");
     printf("Hello World!!!\n");
+    int a;
+
+    char b;
 
     return 0;
 }
